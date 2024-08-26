d18e8157ab6d93f664370b01be1724afc94e8993
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-08-26 20:33:21 +0500

    menu

--- templates/header/parts/menu.php
+++ templates/header/parts/menu.php
@@ -1,14 +1,3 @@
-<?php
-$menu_args = array(
-	'depth'          => 3,
-	'container'      => false,
-	'items_wrap'     => '%3$s',
-	'fallback_cb'    => false,
-	'theme_location' => 'ms-lms-starter-theme-main-menu',
-);
-
-
-?>
 <header
 		class="self-stretch bg-general-white flex flex-row items-center justify-center py-[15px] box-border top-[0] z-[99] sticky max-w-full gap-5 text-right text-base text-general-8-secondary font-subtitles-16  lg:box-border  mq450:box-border mq850:box-border"
 >
@@ -16,60 +5,34 @@ $menu_args = array(
 		<div class="flex flex-row items-center justify-center py-0 px-[3px]">
 			<?php get_template_part( 'templates/header/parts/logo' ); ?>
 		</div>
-		<div
-				class="flex flex-row items-center justify-center py-5 px-0 box-border gap-[39.3px] max-w-full lg:hidden mq850:gap-5"
-		>
-			<div class="flex flex-row items-center justify-start gap-1.5">
-				<a
-						class="[text-decoration:none] relative tracking-[0.04em] leading-[18px] uppercase font-medium text-[inherit] whitespace-nowrap"
-				>Business Setup Services</a
-				>
-				<img
-						class="h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]"
-						alt=""
-						src="/wp-content/themes/vision-prime/web/public/feather-iconschevrondown.svg"
-				/>
-			</div>
-			<div class="flex flex-row items-center justify-start gap-2.5">
-				<a
-						class="[text-decoration:none] relative tracking-[0.04em] leading-[18px] uppercase font-medium text-[inherit] whitespace-nowrap"
-				>Financial Services</a
-				>
-				<img
-						class="h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]"
-						alt=""
-						src="/wp-content/themes/vision-prime/web/public/feather-iconschevrondown.svg"
-				/>
-			</div>
-			<div class="flex flex-row items-center justify-start gap-2.5">
-				<div
-						class="relative tracking-[0.04em] leading-[18px] uppercase font-medium whitespace-nowrap"
-				>
-					Complex Business Support
+		<?php
+		$menu_name = 'ms-lms-starter-theme-main-menu'; // The theme location you specified
+		$locations = get_nav_menu_locations();
+		if ( isset( $locations[ $menu_name ] ) ) {
+			$menu_id    = $locations[ $menu_name ];
+			$menu_items = wp_get_nav_menu_items( $menu_id );
+			if ( $menu_items ) { ?>
+				<div class="flex flex-row items-center justify-center py-5 px-0 box-border gap-[30px] max-w-full lg:hidden mq850:gap-5">
+					<?php
+					foreach ( $menu_items as $menu_item ) :
+						$title = esc_html( $menu_item->title );
+						$url = esc_url( $menu_item->url );
+						?>
+						<div class="flex flex-row items-center justify-start gap-1.5">
+							<a class="[text-decoration:none] relative tracking-[0.04em] leading-[18px] uppercase font-medium text-[inherit] whitespace-nowrap" href="<?php echo $url ?>"> <?php echo $title ?> </a>
+							<img class="h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]" src="/wp-content/themes/vision-prime/web/public/feather-iconschevrondown.svg"/>
+						</div>
+					<?php
+					endforeach;
+					?>
+
 				</div>
-				<img
-						class="h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]"
-						alt=""
-						src="/wp-content/themes/vision-prime/web/public/feather-iconschevrondown.svg"
-				/>
-			</div>
-			<a
-					class="[text-decoration:none] relative tracking-[0.04em] leading-[18px] uppercase font-medium text-[inherit] inline-block min-w-[58px]"
-			>About</a
-			>
-			<a
-					class="[text-decoration:none] relative tracking-[0.04em] leading-[18px] uppercase font-medium text-[inherit] inline-block min-w-[85px] whitespace-nowrap"
-			>Contact
-			</a>
-		</div>
+				<?php
+			}
+		}
+		?>
+
 	</div>
 </header>
-<div class="navigation-menu">
-	<ul class="starter-menu menu">
-		<?php wp_nav_menu( $menu_args ); ?>
-	</ul>
 
-	<div class="mobile-switcher">
-		<span></span> <span></span> <span></span>
-	</div>
-</div>
+
