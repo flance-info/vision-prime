27f24e972785a456f5a2ad0334309adc80cac803
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-09-08 15:48:09 +0500

    Update about_page.php

--- web/about_page.php
+++ web/about_page.php
@@ -78,7 +78,7 @@
 				</div>
 			</div>
 
-			<div class="self-stretch flex flex-col items-start justify-start gap-[60px] max-w-full text-left text-general-9-secondary-variant font-body-b3-merriweather-14 mq1425:gap-[30px]">
+			<div class="self-stretch flex flex-col items-start justify-start gap-[60px] max-w-full text-left text-general-9-secondary-variant font-body-b3-merriweather-14 mq1425:gap-[30px] hidden mq1170:flex">
 				<div class="self-stretch flex flex-row items-start justify-start flex-wrap content-start gap-[30px] max-w-full">
 					<div class="flex-1 relative leading-[26px] inline-block min-w-[370px] max-w-full mq450:min-w-full">
 						<p class="m-[0px]">
@@ -160,7 +160,52 @@
 
 
 			</div>
+			<div class="self-stretch flex flex-row items-center justify-start flex-wrap content-center gap-[40px] text-41xl text-general-1-primary font-button-dm-sans-16 mq950:gap-[20px] mq1170:hidden">
 
+
+				<div class="flex-1 flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px]">
+
+
+					<b class="relative tracking-[-0.03em] leading-[66px] mq450:text-17xl mq450:leading-[40px] mq850:text-29xl mq850:leading-[53px] transition-[_--num] duration-[3s] ease-out [counter-set:_num_var(--num)] supports-[counter-set]:before:content-[counter(num)] [--num:15]"
+					   x-data="{ shown: false }" x-intersect="shown = true" :class="shown &amp;&amp; '[--num:15]'">
+						<span class="supports-[counter-set]:sr-only">15</span>
+					</b>
+					<div class="self-stretch relative text-lg leading-[30px] font-body-b3-merriweather-14 text-general-9-secondary-variant">
+						years of experience
+					</div>
+				</div>
+				<div class="flex-1 flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px]">
+
+
+					<b class="relative tracking-[-0.03em] leading-[66px] mq450:text-17xl mq450:leading-[40px] mq850:text-29xl mq850:leading-[53px] transition-[_--num] duration-[3s] ease-out [counter-set:_num_var(--num)] supports-[counter-set]:before:content-[counter(num)] [--num:100]"
+					   x-data="{ shown: false }" x-intersect="shown = true" :class="shown &amp;&amp; '[--num:100]'">
+						<span class="supports-[counter-set]:sr-only">100</span>+
+					</b>
+					<div class="self-stretch relative text-lg leading-[30px] font-body-b3-merriweather-14 text-general-9-secondary-variant">
+						dedicated team
+					</div>
+				</div>
+				<div class="flex-1 flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px]">
+
+
+					<b class="relative tracking-[-0.03em] leading-[66px] mq450:text-17xl mq450:leading-[40px] mq850:text-29xl mq850:leading-[53px] transition-[_--num] duration-[3s] ease-out [counter-set:_num_var(--num)] supports-[counter-set]:before:content-[counter(num)] [--num:2500]"
+					   x-data="{ shown: false }" x-intersect="shown = true" :class="shown &amp;&amp; '[--num:2500]'">
+						<span class="supports-[counter-set]:sr-only">2500</span>+
+					</b>
+					<div class="self-stretch relative text-lg leading-[30px] font-body-b3-merriweather-14 text-general-9-secondary-variant">
+						consultations
+					</div>
+				</div>
+				<div class="flex-1 flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px] text-center">
+					<b class="relative tracking-[-0.03em] leading-[66px] mq450:text-17xl mq450:leading-[40px] mq850:text-29xl mq850:leading-[53px] transition-[_--num] duration-[3s] ease-out [counter-set:_num_var(--num)] supports-[counter-set]:before:content-[counter(num)] [--num:5000]"
+					   x-data="{ shown: false }" x-intersect="shown = true" :class="shown &amp;&amp; '[--num:5000]'">
+						<span class="supports-[counter-set]:sr-only">5000</span>+
+					</b>
+					<div class="self-stretch relative text-lg leading-[30px] font-body-b3-merriweather-14 text-general-9-secondary-variant text-left">
+						audit completed
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 
