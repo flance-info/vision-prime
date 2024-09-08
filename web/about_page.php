6640d82c85ec63ea876d781106e88f25f3f42852
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-09-08 15:44:43 +0500

    history mission

--- web/about_page.php
+++ web/about_page.php
@@ -111,10 +111,10 @@
 						</p>
 					</div>
 				</div>
-				<div class="self-stretch flex flex-row items-center justify-start flex-wrap content-center gap-[40px] text-41xl text-general-1-primary font-button-dm-sans-16 mq950:gap-[20px]">
+				<div class="self-stretch flex flex-row items-center justify-start flex-wrap content-center gap-[40px] text-41xl text-general-1-primary font-button-dm-sans-16 mq950:gap-[10px]  mq950:gap-x-[5px] mq950:items-start   items-start mq650:gap-x-[0px]">
 
 
-					<div class="flex-1 flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px] mq650:w-[50%] mq650:min-w-[100px]">
+					<div class=" flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px] mq650:w-[50%] mq950:w-[23%] mq650:min-w-[100px]  mq950:min-w-[150px]">
 
 
 						<b class="relative tracking-[-0.03em] leading-[66px] mq450:text-17xl mq450:leading-[40px] mq850:text-29xl mq850:leading-[53px] transition-[_--num] duration-[3s] ease-out [counter-set:_num_var(--num)] supports-[counter-set]:before:content-[counter(num)] [--num:15]"
@@ -125,7 +125,7 @@
 							years of experience
 						</div>
 					</div>
-					<div class="flex-1 flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px] mq650:w-[50%] mq650:min-w-[100px]">
+					<div class=" flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px] mq650:w-[50%] mq950:w-[23%] mq650:min-w-[100px]  mq950:min-w-[150px] ">
 
 
 						<b class="relative tracking-[-0.03em] leading-[66px] mq450:text-17xl mq450:leading-[40px] mq850:text-29xl mq850:leading-[53px] transition-[_--num] duration-[3s] ease-out [counter-set:_num_var(--num)] supports-[counter-set]:before:content-[counter(num)] [--num:100]"
@@ -136,7 +136,7 @@
 							dedicated team
 						</div>
 					</div>
-					<div class="flex-1 flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px] mq650:w-[50%] mq650:min-w-[100px]">
+					<div class=" flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px] mq650:w-[50%] mq950:w-[23%] mq650:min-w-[100px]  mq950:min-w-[150px]">
 
 
 						<b class="relative tracking-[-0.03em] leading-[66px] mq450:text-17xl mq450:leading-[40px] mq850:text-29xl mq850:leading-[53px] transition-[_--num] duration-[3s] ease-out [counter-set:_num_var(--num)] supports-[counter-set]:before:content-[counter(num)] [--num:2500]"
@@ -147,7 +147,7 @@
 							consultations
 						</div>
 					</div>
-					<div class="flex-1 flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px] mq650:w-[50%] mq650:min-w-[100px] text-center">
+					<div class=" flex flex-col items-start justify-start gap-[10px] min-w-[206px] max-w-[262px] mq650:w-[50%] mq950:w-[23%] mq650:min-w-[100px] text-center  mq950:min-w-[150px]">
 						<b class="relative tracking-[-0.03em] leading-[66px] mq450:text-17xl mq450:leading-[40px] mq850:text-29xl mq850:leading-[53px] transition-[_--num] duration-[3s] ease-out [counter-set:_num_var(--num)] supports-[counter-set]:before:content-[counter(num)] [--num:5000]"
 						   x-data="{ shown: false }" x-intersect="shown = true" :class="shown &amp;&amp; '[--num:5000]'">
 							<span class="supports-[counter-set]:sr-only">5000</span>+
@@ -157,6 +157,8 @@
 						</div>
 					</div>
 				</div>
+
+
 			</div>
 
 		</div>
